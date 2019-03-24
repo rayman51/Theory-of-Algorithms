c28d599aa290211bab78ade67da95954250d076f
rayman51 <ray_mannionit2@theory-of-algorithms.europe-west2-c.c.raymannionproject.internal>
2019-03-24 20:21:21 +0000

    padding video 2

--- padfile.c
+++ padfile.c
@@ -1,45 +1,76 @@
-# include <stdio.h>
-# include <stdint.h>
+#include <stdio.h>
+#include <stdint.h>
 
-union msgblock{
-    uint8_t  e[64];
-    uint32_t t[16];
-    uint64_t s[8];
-};// msgblock
+union msgblock {
+  uint8_t  e[64];
+  uint32_t t[16];
+  uint64_t s[8];
+};
+
+enum status {READ , PAD0, PAD1, FINISH};
 
 int main(int argc, char *argv[]){
 
-    union msgblock M;
+  union msgblock M;
 
-    uint64_t nobits = 0;
-    uint64_t nobytes;
+  uint64_t nobits = 0;
 
-    FILE* f;
-    f = fopen(argv[1],"r");
+  uint64_t nobytes;
 
-    while (!feof(f)){
-        nobytes = fread(M.e, 1, 64, f); 
-        nobits = nobits + (nobytes * 8);
+  enum status S = READ;
 
-        if (nobytes < 56){
-            printf("I've found a block with less than 55 bytes!\n");
-            M.e[nobytes] = 0x80;
+  FILE* f;
 
-            while(nobytes < 56){
-                nobytes = nobytes + 1;
-                M.e[nobytes] = 0x00;
-            }// while
+  f = fopen(argv[1], "r");
+  int i;
 
-            M.e[7] = nobits;
-        }// if
+  while (S == READ) {
+    nobytes = fread(M.e, 1, 64, f);
+    printf("Read %2llu bytes \n", nobytes);
+    nobits = nobits + (nobytes * 8);
 
-    }// while
+    if (nobytes < 56){
+      printf("I've found a block with less than 55 bytes !\n");
+      M.e[nobytes] = 0x80;
+      while (nobytes < 56) {
+        nobytes = nobytes + 1;
+        M.e[nobytes] = 0x00; 
+      }//while
 
-    fclose(f);
+      M.s[7] = nobits; 
+      S = FINISH;
+    }//if
+    else if (nobytes < 64) {
+      S = PAD0;
+      M.e[nobytes] = 0x80;
+      while (nobytes < 64) {
+          nobytes = nobytes + 1;
+          M.e[nobytes] = 0x00; 
+        }//while
+    }//else if
 
-    for(int i = 0; i < 64; i++)
-        printf("%x ", M.e[i]);
-        printf("\n");
-   
-    return 0; 
-}// main
+    // Has everything finished reading from file and was it a multiple of 512
+    else if (feof(f)){
+      S = PAD1;
+    }//else if 
+  }//while
+
+  if (S == PAD0 || S == PAD1){
+    for (i = 0; i < 56; i++) {
+          M.e[i] = 0x00;
+    }     
+     M.s[7] = nobits; 
+    
+  }
+  if (S == PAD1){
+    M.e[7]= 0x80;
+  }
+
+  fclose(f);
+
+  for (int i = 0; i < 64; i++)
+    printf("%x ",M.e[i]);
+    printf("\n");
+
+  return 0 ;
+}//main
